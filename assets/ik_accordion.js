52cb8e56d43f549cc23b70943b3cd5f56cf319ef
Shawn Turple <sturple@sturple-deb.hitronhub.home>
2018-10-15 12:58:45 -0700

    Activity 9 Accessible Accordion

--- assets/ik_accordion.js
+++ assets/ik_accordion.js
@@ -1,11 +1,11 @@
 ;(function ( $, window, document, undefined ) {
- 	
+
 	var pluginName = 'ik_accordion',
 		defaults = { // set default parameters
 			autoCollapse: false,
 			animationSpeed: 200
 		};
-	 
+
 	/**
 	 * @constructs Plugin
 	 * @param {Object} element - Current DOM element from selected collection.
@@ -14,52 +14,64 @@
 	 * @param {number} options.animationSpeed - Panel toggle speed in milliseconds.
 	 */
 	function Plugin( element, options ) {
-		
+
 		this._name = pluginName;
 		this._defaults = defaults;
 		this.element = $(element);
 		this.options = $.extend( {}, defaults, options) ; // override default parameters if setup object is present
-		
+
 		this.init();
 	}
-	
+
 	/** Initializes plugin. */
 	Plugin.prototype.init = function () {
-		
+
 		var id, $elem, plugin;
-		
+
 		id = 'acc' + $('.ik_accordion').length; // create unique id
 		$elem = this.element;
 		plugin = this;
-		
+
 		$elem.attr({
-			'id': id
+			'id': id,
+            'aria-multiselectable': !this.options.autoCollapse,
+            'role': 'region' // add the accordion to the landmarked regions
 		}).addClass('ik_accordion');
-			
+
 		this.headers = $elem.children('dt').each(function(i, el) {
 			var $me, $btn;
-			
+
 			$me = $(el);
 			$btn = $('<div/>').attr({
-          'id': id + '_btn_' + i
+          'id': id + '_btn_' + i,
+          'role': 'button',
+          'aria-controls': id + '_panel_' + i, // associate button with corresponding panel
+          'aria-expanded': false, // toggle expanded state
+          'tabindex': 0 //add keyboard focus
         })
+
         .addClass('button')
         .html($me.html())
+        .on('keydown', {'plugin': plugin}, plugin.onKeyDown) // enable keyboard navigation
         .on('click', {'plugin': plugin}, plugin.togglePanel);
-        
+
 			$me.empty().append($btn); // wrap content of each header in an element with role button
-		});
-		
+		})
+        .attr({ 'role' : 'heading'});
+
 		this.panels = $elem.children('dd').each(function(i, el) {
 			var $me = $(this), id = $elem.attr('id') + '_panel_' + i;
 			$me.attr({
-				'id': id
+				'id': id,
+                'role': 'region', // add role region to each panel
+                'aria-hidden': true, // mark all panels as hidden
+                'tabindex': 0 // add panels into the tab order
 			});
 		}).hide();
-		
+
 	};
-	
-	/** 
+
+	/**
 	 * Toggles accordion panel.
 	 *
 	 * @param {Object} event - Keyboard or mouse event.
@@ -67,50 +79,111 @@
 	 * @param {object} event.data.plugin - Reference to plugin.
 	 */
 	Plugin.prototype.togglePanel = function (event) {
-		
+
 		var plugin, $elem, $panel, $me, isVisible;
-		
+
 		plugin = event.data.plugin;
 		$elem = $(plugin.element);
 		$me = $(event.target);
 		$panel = $me.parent('dt').next();
-		
+
 		if(plugin.options.autoCollapse) { // expand current panel and collapse the rest
-			
+
 			plugin.headers.each(function(i, el) {
-				var $hdr, $btn; 
-				
+				var $hdr, $btn;
+
 				$hdr = $(el);
 				$btn = $hdr.find('.button');
-				
-				if($btn[0] != $(event.currentTarget)[0]) { 
-					$btn.removeClass('expanded');
-					$hdr.next().slideUp(plugin.options.animationSpeed);
-				} else { 
-					$btn.addClass('expanded');
-					$hdr.next().slideDown(plugin.options.animationSpeed);
+
+				if($btn[0] != $(event.currentTarget)[0]) {
+					$btn
+                        .removeClass('expanded')
+                        .attr('aria-expanded', 'false');
+                    $hdr.next()
+                        .attr('aria-hidden', 'true')
+                        .slideUp(plugin.options.animationSpeed);
+				} else {
+					$btn
+                        .addClass('expanded')
+                        .attr('aria-expanded', 'true')
+                    $hdr.next()
+                        .attr('aria-hidden', 'false')
+                        .slideDown(plugin.options.animationSpeed);
 				}
 			});
-			
+
 		} else { // toggle current panel depending on the state
-		
 			isVisible = !!$panel.is(':visible');
 			$panel.slideToggle({ duration: plugin.options.animationSpeed });
-			
+            if ($me.attr('aria-expanded') == 'false') {
+                $me.attr('aria-expanded', 'true')
+                $panel.attr('aria-hidden', 'false')
+            }
+            else {
+                $me.attr('aria-expanded', 'false')
+                $panel.attr('aria-hidden', 'true')
+            }
 		}
+
+
 	};
-	
+
+    /**
+         * Handles kedown event on header button.
+         *
+         * @param {Object} event - Keyboard event.
+         * @param {object} event.data - Event data.
+         * @param {object} event.data.plugin - Reference to plugin.
+         */
+        Plugin.prototype.onKeyDown = function (event) {
+
+            var $me, $header, plugin, $elem, $current, ind;
+
+            $me = $(event.target);
+            $header = $me.parent('dt');
+            plugin = event.data.plugin;
+            $elem = $(plugin.element);
+
+            switch (event.keyCode) {
+
+                // toggle panel by pressing enter key, or spacebar
+                case ik_utils.keys.enter:
+                case ik_utils.keys.space:
+                    event.preventDefault();
+                    event.stopPropagation();
+                    plugin.togglePanel(event);
+                    break;
+
+                // use up arrow to jump to the previous header
+                case ik_utils.keys.up:
+                    ind = plugin.headers.index($header);
+                    if (ind > 0) {
+                        plugin.headers.eq(--ind).find('.button').focus();
+                    }
+                    console.log(ind);
+                    break;
+
+                // use down arrow to jump to the next header
+                case ik_utils.keys.down:
+                    ind = plugin.headers.index($header);
+                    if (ind < plugin.headers.length - 1) {
+                        plugin.headers.eq(++ind).find('.button').focus();
+                    }
+                    break;
+            }
+        };
+
 	$.fn[pluginName] = function ( options ) {
-		
+
 		return this.each(function () {
-			
+
 			if ( !$.data(this, pluginName )) {
 				$.data( this, pluginName,
 				new Plugin( this, options ));
 			}
-			
+
 		});
-		
+
 	}
- 
+
 })( jQuery, window, document );
